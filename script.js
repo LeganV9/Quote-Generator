53363a3683b4ec03dfef9c0bd0910a303836d248
LeganV9 <yanisrenaud77@gmail.com>
2023-03-01 23:22:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // check if author field is blank and replace it with 'Unknown'
-    if (!quote.author) {  // exclamation mark here line 14 says "if there is NO quote.author here..."
+    if (!quote.author) {  //> exclamation mark here line 14 says "if there is NO quote.author here..."
         authorText.textContent = 'Unknown';
     } else {
         authorText.textContent = quote.author;
     }
     // check quote length to determine styling
-    if (quote.text.length > 50) {
+    if (quote.text.length > 120) {
         quoteText.classList.add('long-quote');
     } else {
         quoteText.classList.remove('long-quote');
@@ -38,6 +38,15 @@ async function getQuotes() {
     }
 }
 
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank'); //> so that it opens in a new tab
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
 getQuotes();
-
